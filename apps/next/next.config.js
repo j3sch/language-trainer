d8f8bd1e5955cb7644b9c8c14e6be6bb48fa3e34
Jens Schlegel <39447994+j3sch@users.noreply.github.com>
2023-08-18 22:26:30 +0200

    update nextjs config

--- apps/next/next.config.js
+++ apps/next/next.config.js
@@ -1,8 +1,27 @@
-const nextTranslate = require('next-translate-plugin')
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-	reactStrictMode: true,
+const boolVals = {
+  true: true,
+  false: false,
 }
 
-module.exports = nextTranslate(nextConfig)
+const disableBrowserLogs =
+  boolVals[process.env.DISABLE_BROWSER_LOGS] ?? process.env.NODE_ENV === 'production'
+
+module.exports = function () {
+  /** @type {import('next').NextConfig} */
+  let config = {
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    experimental: {
+      appDir: false,
+      forceSwcTransforms: true,
+      scrollRestoration: true,
+      legacyBrowsers: false,
+    },
+    compiler: {
+      removeConsole: disableBrowserLogs,
+    },
+  }
+
+  return config
+}
