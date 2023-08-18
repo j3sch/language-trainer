375fd6010298a354fa33d032679c61552102b865
Jens Schlegel <39447994+j3sch@users.noreply.github.com>
2023-08-18 15:03:20 +0200

    remove unsued imports

--- apps/next/src/pages/favourites.tsx
+++ apps/next/src/pages/favourites.tsx
@@ -1,22 +1,6 @@
-import TranslateInput from 'src/components/TranslateInput'
 import { api } from 'src/utils/api'
-import { type KeyboardEvent, type MouseEvent, useState, useEffect } from 'react'
-import { markWords } from 'src/utils/algo'
 import Footer from 'src/components/Footer'
-import { ArrowRightOnRectangleIcon, ArrowsRightLeftIcon } from '@heroicons/react/24/outline'
-import { clsx } from 'clsx'
-import LanguageSelect from 'src/components/LanguageSelect'
-import TenseSelect from 'src/components/TenseSelect'
-import CompleteHistory from 'src/components/CompleteHistory'
-import { History } from 'src/types'
-import { isResTypeCorrect } from 'src/utils/isResTypeCorrect'
-import { LANGUAGES } from 'src/types/languages'
-import { getUser } from '@/utils/supabase'
-import Settings from '@/components/Settings'
-import { useAnswerLanguage, useQuestionLanguage } from '@/atoms/settings'
 import SidebarNavigation from '@/components/SidebarNavigation'
-import SolutionBox from '@/components/SolutionBox'
-import { useAtom } from 'jotai'
 
 export default function Favourites() {
 	const { data, refetch } = api.translations.getHistories.useQuery()
