929423970da619571970bad900b7ad6eb6319cbd
Jens Schlegel <39447994+j3sch@users.noreply.github.com>
2023-08-24 12:30:50 +0200

    update verify email color

--- apps/next/src/pages/verify-email.tsx
+++ apps/next/src/pages/verify-email.tsx
@@ -7,7 +7,7 @@ export default function VerifyEmail() {
 
   return (
     <div className="h-full w-full flex justify-center items-center flex-col space-y-5">
-      <InboxArrowDownIcon className="h-28 w-28 text-blue-500" />
+      <InboxArrowDownIcon className="h-28 w-28 dark:text-indigo-400" />
       <h1 className="text-4xl font-bold">Verify your email address</h1>
       <div className="flex items-center flex-col space-y-1">
         <p>We have sent a verification link to {query.email}.</p>
